16f56e20fbf6e4b7ec105a8cdf674eabb7f8e00d
ChuanhaoHuang <hchdggyouxiang@gmail.com>
2020-03-22 21:53:19 +0800

    wo de diyici  xiugai

--- venv/Include/KNN.py
+++ venv/Include/KNN.py
@@ -10,3 +10,13 @@ import matplotlib.pyplot as plt
 plt.rcParams['figure.figsize'] = (10.0, 8.0) # set default size of plots
 plt.rcParams['image.interpolation'] = 'nearest'
 plt.rcParams['image.cmap'] = 'gray'
+
+# Load the raw CIFAR-10 data.
+cifar10_dir = 'cs231n/datasets/cifar-10-batches-py'
+X_train, y_train, X_test, y_test = load_CIFAR10(cifar10_dir)
+
+# As a sanity check, we print out the size of the training and test data.
+print('Training data shape: ', X_train.shape)
+print('Training labels shape: ', y_train.shape)
+print('Test data shape: ', X_test.shape)
+print('Test labels shape: ', y_test.shape)
