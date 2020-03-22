0e4df15c0949945f180708f62754b3260f0e9af2
ChuanhaoHuang <hchdggyouxiang@gmail.com>
2020-03-22 21:51:16 +0800

    wo de diyici  xiugai

--- venv/Include/KNN.py
+++ venv/Include/KNN.py
@@ -4,3 +4,9 @@ import random
 import numpy as np
 from cs231n.data_utils import load_CIFAR10
 import matplotlib.pyplot as plt
+# This is a bit of magic to make matplotlib figures appear inline in the notebook
+# rather than in a new window.
+#%matplotlib inline
+plt.rcParams['figure.figsize'] = (10.0, 8.0) # set default size of plots
+plt.rcParams['image.interpolation'] = 'nearest'
+plt.rcParams['image.cmap'] = 'gray'
